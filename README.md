d932fa28ca7771bda09e6755b13b3c8badab01f2
Akhiril Anwar Harahap <107613389+akhirilanwarharahap@users.noreply.github.com>
2024-06-26 17:32:29 +0700

    update all

--- README.md
+++ README.md
@@ -1,14 +1,41 @@
-# Dicoding Collection Dashboard ✨
+# Belajar Analisis Data dengan Python
 
-## Setup environment
-```
-conda create --name main-ds python=3.9
-conda activate main-ds
-pip install numpy pandas scipy matplotlib seaborn jupyter streamlit babel
-```
+## Deskripsi
 
-## Run steamlit app
-```
-streamlit run dashboard.py
-```
+Proyek ini bertujuan untuk menganalisis data pada Bike Sharing Dataset. Tujuan akhirnya adalah untuk menghasilkan wawasan dan informasi yang berguna dari data yang dianalisis.
+
+## Struktur Direktori
+
+- **/dashboard**: Direktori ini berisi dashboard.py yang digunakan untuk membuat dashboard hasil analisis data.
+- **/data**: Direktori ini berisi data yang digunakan dalam proyek, dalam format .csv .
+- **Proyek_Analisis_Data_E_Commerce.ipynb**: File ini yang digunakan untuk melakukan analisis data.
+
+## Instalasi
+
+1. Clone repository ini ke komputer lokal Anda menggunakan perintah berikut:
+
+   ```shell
+   git clone https://github.com/akhirilanwarharahap/Proyek-Analisis-Data-Dicoding
+   ```
+
+2. Pastikan Anda memiliki environment Python yang sesuai dan pustaka-pustaka yang diperlukan. Anda dapat menginstal pustaka-pustaka tersebut dengan menjalankan perintah berikut:
+
+   ```shell
+   pip install streamlit
+   pip install -r dashboard/requirements.txt
+   ```
+
+## Penggunaan
+
+1. Masuk ke direktori proyek (Local):
+
+   ```shell
+   cd dashboard
+   streamlit run main.py
+   ```
+
+   Atau bisa dengan kunjungi website ini [Project Data Analytics](https://dicoding-tjan2lwsgz3l97a447szqk.streamlit.app/)
+
+   <img src="./dashboard/ss.png" alt="Streamlit logo"></img>
+
 
--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,88 +5,75 @@ import streamlit as st
 
 sns.set(style="dark")
 
-
 # Load data from CSV
 all_df = pd.read_csv("all_data.csv")
 
 # Konversi kolom 'dteday' menjadi datetime jika diperlukan
 all_df["dteday"] = pd.to_datetime(all_df["dteday"])
 
+min_date = all_df["dteday"].min()
+max_date = all_df["dteday"].max()
 
-if all_df is not None:
-    min_date = all_df["dteday"].min()
-    max_date = all_df["dteday"].max()
+with st.sidebar:
+    st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
 
-    with st.sidebar:
-        st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
+    start_date, end_date = st.date_input(
+        label="Rentang Waktu",
+        min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date],
+    )
 
-        start_date, end_date = st.date_input(
-            label="Rentang Waktu",
-            min_value=min_date,
-            max_value=max_date,
-            value=[min_date, max_date],
-        )
+main_df = all_df[
+    (all_df["dteday"] >= str(start_date)) & (all_df["dteday"] <= str(end_date))
+]
 
-    main_df = all_df[
-        (all_df["dteday"] >= str(start_date)) & (all_df["dteday"] <= str(end_date))
-    ]
+# Visualisasi Monthly Count of Rental
+st.header("Dicoding Collection Dashboard :sparkles:")
+st.subheader("Monthly Count of Rental")
 
-    # Visualisasi Total Orders by Weekday
-    st.header("Dicoding Collection Dashboard :sparkles:")
-    st.subheader("Total Orders by Weekday")
+# Resample data by month and sum the counts
+monthly_orders_df = main_df.resample(rule="M", on="dteday").agg({"cnt": "sum"})
+monthly_orders_df.index = monthly_orders_df.index.strftime("%Y-%m")
+monthly_orders_df = monthly_orders_df.reset_index()
+monthly_orders_df.rename(columns={"cnt": "revenue"}, inplace=True)
 
-    if "cnt" in main_df.columns:
-        # Grup berdasarkan weekday, menjumlahkan cnt, kemudian mengurutkan
-        main_df["weekday"] = main_df["dteday"].dt.day_name()
-        weekday_counts = (
-            main_df.groupby("weekday")
-            .agg({"cnt": "sum"})
-            .sort_values(by="cnt", ascending=False)
-        )
+fig, ax = plt.subplots(figsize=(10, 6))  # Mengatur ukuran plot
+sns.lineplot(x="dteday", y="revenue", data=monthly_orders_df, marker="o", ax=ax)
+ax.set_title("Monthly Count of Rental")  # Judul plot
+ax.set_xlabel("Month")  # Label sumbu x
+ax.set_ylabel("Count of Rental")  # Label sumbu y
+plt.xticks(rotation=45)  # Memutar label sumbu x agar tidak bertabrakan
+plt.tight_layout()  # Menyesuaikan layout agar tidak tumpang tindih
 
-        fig, ax = plt.subplots(figsize=(10, 6))  # Mengatur ukuran plot
-        sns.lineplot(x=weekday_counts.index, y=weekday_counts["cnt"], marker="o", ax=ax)
-        ax.set_title("Total Orders by Weekday")  # Judul plot
-        ax.set_xlabel("Weekday")  # Label sumbu x
-        ax.set_ylabel("Total Orders")  # Label sumbu y
-        plt.xticks(rotation=45)  # Memutar label sumbu x agar tidak bertabrakan
-        plt.tight_layout()  # Menyesuaikan layout agar tidak tumpang tindih
+st.pyplot(fig)
 
-        st.pyplot(fig)
-    else:
-        st.error("Column 'cnt' not found in the dataset.")
+# Visualisasi Total Orders by Season
+st.subheader("Total Orders by Season")
 
-    # Visualisasi Total Orders by Season
-    st.subheader("Total Orders by Season")
+# Grup berdasarkan season, menjumlahkan cnt
+season_counts = main_df.groupby("season").agg({"cnt": "sum"})
 
-    if "season" in main_df.columns:
-        # Grup berdasarkan season, menjumlahkan cnt
-        season_counts = main_df.groupby("season").agg({"cnt": "sum"})
+fig, ax = plt.subplots(figsize=(4, 6))  # Mengatur ukuran plot
+bars = ax.bar(season_counts.index, season_counts["cnt"], color="skyblue")
+ax.set_title("Total Orders by Season")  # Judul plot
+ax.set_xlabel("Season")  # Label sumbu x
+ax.set_ylabel("Total Orders")  # Label sumbu y
+plt.xticks(rotation=0)  # Memutar label sumbu x agar tidak bertabrakan
+plt.tight_layout()  # Menyesuaikan layout agar tidak tumpang tindih
 
-        fig, ax = plt.subplots(figsize=(4, 6))  # Mengatur ukuran plot
-        bars = ax.bar(season_counts.index, season_counts["cnt"], color="skyblue")
-        ax.set_title("Total Orders by Season")  # Judul plot
-        ax.set_xlabel("Season")  # Label sumbu x
-        ax.set_ylabel("Total Orders")  # Label sumbu y
-        plt.xticks(rotation=0)  # Memutar label sumbu x agar tidak bertabrakan
-        plt.tight_layout()  # Menyesuaikan layout agar tidak tumpang tindih
+# Menambahkan label nilai di atas setiap bar
+for bar in bars:
+    yval = bar.get_height()  # Mendapatkan tinggi bar
+    ax.text(
+        bar.get_x() + bar.get_width() / 2,
+        yval,
+        int(yval),
+        va="bottom",
+        ha="center",
+        fontsize=11,
+    )
 
-        # Menambahkan label nilai di atas setiap bar
-        for bar in bars:
-            yval = bar.get_height()  # Mendapatkan tinggi bar
-            ax.text(
-                bar.get_x() + bar.get_width() / 2,
-                yval,
-                int(yval),
-                va="bottom",
-                ha="center",
-                fontsize=11,
-            )
+st.pyplot(fig)
 
-        st.pyplot(fig)
-    else:
-        st.error("Column 'season' not found in the dataset.")
-
-    st.caption("Copyright © Dicoding 2023")
-else:
-    st.error("Data not loaded successfully.")
+st.caption("Copyright © Dicoding 2023")
